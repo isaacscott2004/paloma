e3f719032b3f2f229bb8c427c28bb7b0c3f3468e
Isaac Irwin <isaacscottirwin@gmail.com>
2025-07-23 12:37:46 -0600

    added java server module

---
+++ javaServer/src/test/java/com/paloma/paloma/javaServer/JavaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paloma.paloma.javaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
