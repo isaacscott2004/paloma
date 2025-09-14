ac95e61984b94db9923f30d8322af4a3d2f69fde
Isaac Irwin <isaacscottirwin@gmail.com>
2025-09-13 19:23:05 -0600

    test

--- javaServer/src/main/java/com/paloma/paloma/javaServer/configurations/SecurityConfig.java
+++ javaServer/src/main/java/com/paloma/paloma/javaServer/configurations/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 public class SecurityConfig {
+    
 
     private final JwtAuthFilter jwtAuthFilter;
 
